ec014ab97e0da4bb046ce027b156a608040ee01a
Włodzimierz Pavlov <pavlovcoder@gmail.com>
2019-06-18 20:53:10 +0200

    v0.0.1
    Added a few basic startup files, tools and libraries.

---
+++ tools/startup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+print('checking for nltk')
+try:
+    import nltk
+except ImportError:
+    print('you should isntall nltk before continuing')
+
+print('checking for numpy')
+try:
+    import numpy
+except ImportError:
+    print('you should install numpy before continuing')
+
+print('checkking for scipy')
+try:
+    import scipy
+except:
+    print('you shuold install scipy before continuing')
+
+print("checking for sklearn")
+print('checking for sklearn')
+try:
+    import sklearn
+except:
+    print("you should install sklearn before continuing")
+
+print("downloading the Enron dataset (this may take a while)")
+print("to check on progress, you can cd up one level, then execute <ls -lthr>")
+print("Enron dataset should be last item on the list, along with its current size")
+print("download will complete at about 423 MB")
+import urllib.request, urllib.parse, urllib.error
+url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
+urllib.request.urlretrieve(url, filename="../enron_mail_20150507.tar.gz") 
+print("download complete!")
+
+print("unzipping Enron dataset (this may take a while)")
+import tarfile
+import os
+os.chdir("..")
+tfile = tarfile.open("enron_mail_20150507.tar.gz", "r:gz")
+tfile.extractall(".")
+
+print("you're ready to go!")
